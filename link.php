60afa326e78f71f5462dafd7fd20f177bc9e358a
turtles2 <jenosdeaddog@gmail.com>
2016-03-10 18:15:50 -0600

    Map links

--- link.php
+++ link.php
@@ -93,6 +93,7 @@ echo'
           </ul></li>
             <li><a href="createsite.php">Create a Site</a></li>
             <li><a href="mapsites.php">Map Sites</a></li>
+            <li><a href="maplinks.php">Map Links</a></li>
             <li><a href="drawcova.php">Draw Coverage</a></li>
             <li><a href="mapcov.php">Map Coverage</a></li>
             <li><a href="linkgear.php">Link a Device to a Site</a></li>
